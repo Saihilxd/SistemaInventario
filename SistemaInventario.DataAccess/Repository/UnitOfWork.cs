590d99e15812e228a2e4f64d0a9f01fc38d05f9f
saihilxd <abisaiortiz2000@gmail.com>
2023-11-12 21:23:03 -0600

    Patron de repositorio

---
+++ SistemaInventario.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using SistemaInventario.DataAccess.Data;
+using SistemaInventario.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IBodegaRepository Bodega { get; private set; }
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            Bodega = new BodegaRepository(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
